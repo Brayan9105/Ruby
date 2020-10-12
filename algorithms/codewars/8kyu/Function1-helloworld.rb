727843ff5dc9bb8942b45d1fa9efea92bc9e2ec6
Brayan lopez <brayan9105@hotmail.com>
2020-10-11 23:28:52 -0500

    Added hello world solution

---
+++ algorithms/codewars/8kyu/Function1-helloworld.rb
@@ -0,0 +1,18 @@
+=begin
+Description:
+
+Make a simple function called greet that returns the most-famous "hello world!".
+Style Points
+
+Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+
+describe "Testing function" do
+  it "Correct return-value?" do
+    Test.assert_equals(greet, "hello world!")
+  end
+end
+=end
+
+def greet
+  "hello world!"
+end
