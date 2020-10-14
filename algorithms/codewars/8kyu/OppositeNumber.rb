36399a8cec399c40de4642ec90ee2ee3eda0506a
Brayan lopez <brayan9105@hotmail.com>
2020-10-14 14:04:30 -0500

    Added Opposite number solution

---
+++ algorithms/codewars/8kyu/OppositeNumber.rb
@@ -0,0 +1,20 @@
+=begin
+  Very simple, given a number, find its opposite.
+
+  Examples:
+
+  1: -1
+  14: -14
+  -34: 34
+
+  Test.assert_equals(opposite(1), -1, msg = nil)
+
+=end
+
+def opposite n
+ n * -1
+end
+
+def opposite n
+  -n
+end
